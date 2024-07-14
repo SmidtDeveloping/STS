20a1886de36f77da30a82e5f678e54ef3959a295
SmidtDeveloping <101642623+SmidtDeveloping@users.noreply.github.com>
2024-07-14 20:59:52 +0200

    Update ready.js

--- handelers/events/ready.js
+++ handelers/events/ready.js
@@ -17,16 +17,21 @@ module.exports = {
 				console.log("✅| Database")
 			)
 			const activities = [
-				{ name: 'SJAKIEE!!', type: ActivityType.Listening },
+				{ name: 'STS!!', type: ActivityType.Listening },
 				{ name: 'GEMAAKT DOOR JULIANRJC', type: ActivityType.Listening },
+				{name: "Website", type: ActivityType.Streaming, url: "https://developingbyjulian.nl"}
 			];
 		// client.user.setActivity('MONSTERGANG!!', { type: ActivityType.Listening })
 
 		function setRandomActivity() {
 			const randomActivity = activities[Math.floor(Math.random() * activities.length)];
-			client.user.setActivity(randomActivity.name, { type: randomActivity.type });
+			if (randomActivity.type === ActivityType.Listening) {
+				client.user.setActivity(randomActivity.name, { type: randomActivity.type });
+			} else if (randomActivity.type === ActivityType.Streaming) {
+				client.user.setActivity({name: randomActivity.name, type: randomActivity.type, url: randomActivity.url});
+			}
 		}
-		client.user.setPresence({activites: [{name: 'SJAKIE '}], status: "online"})
+		client.user.setPresence({activites: [{name: 'STS '}], status: "online"})
 
 		// Stel de activiteit in wanneer de bot voor het eerst online komt
 		setRandomActivity();
